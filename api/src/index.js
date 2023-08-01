27ba48171aa72b738711ea8d51e213d2853b0398
Artem Sychov <[artem.suchov@gmail.com]>
2023-08-01 21:37:49 +0200

    finish with volumes and dev nodemon

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {connectDb} = require("./helpers/db");
+const {host, port, db } = require("./configuration");
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST;
+const postSchema = new mongoose.Schema({name: String});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`Started api service on port: ${port}`);
+        console.log(`On host: ${host}`);
+        console.log(`Our db: ${db}`);
+
+        //New way ( use this in your projects )
+
+        const silence = new Post({name: "Silence" });
+        (async () => {
+            silence = await data.save();
+        })
+        console.log(silence);
+        console.log("save done!");
+    });
+};
 
 app.get("/test", (req, res) => {
     res.send("Our api server is working correctly");
 });
 
-app.listen(port, () => {
-    console.log(`Started api service on port: ${port}`);
-    console.log(`On host: ${host}`);
-});
+connectDb()
+    .on('error', console.log)
+    .on('disconnected', connectDb)
+    .once('open', startServer);
